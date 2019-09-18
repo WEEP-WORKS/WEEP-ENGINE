50b08db103c272672b6564e5447bf2ecb0b586b0
Jorge <jgherencia@gmail.com>
2019-09-18 17:40:35 +0200

    Cleaning project

--- WeepEngine/ModuleSceneIntro.cpp
+++ WeepEngine/ModuleSceneIntro.cpp
@@ -34,9 +34,9 @@ bool ModuleSceneIntro::CleanUp()
 // Update
 update_status ModuleSceneIntro::Update(float dt)
 {
-	Plane p(0, 1, 0, 0);
-	p.axis = true;
-	p.Render();
+	//Plane p(0, 1, 0, 0);
+//	p.axis = true;
+	//p.Render();
 
 	return UPDATE_CONTINUE;
 }
