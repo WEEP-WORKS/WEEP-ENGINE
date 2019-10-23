eae357bc0208af2e8d73cc8ae684f75cb639f74f
youis11 <lluis.moreu@gmail.com>
2019-10-23 18:13:56 +0200

    checkbox to change checkers WIP

--- Weep Engine/ModuleGameObjectManager.cpp
+++ Weep Engine/ModuleGameObjectManager.cpp
@@ -7,6 +7,7 @@
 #include "ComponentMesh.h"
 #include "par_shapes.h"
 #include "ModuleTexture.h"
+#include "ComponentTexture.h"
 
 GameObjectManager::GameObjectManager(bool start_enabled) : Module(start_enabled)
 {
@@ -16,10 +17,7 @@ GameObjectManager::GameObjectManager(bool start_enabled) : Module(start_enabled)
 
 bool GameObjectManager::Update() //dt?
 {
-	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)  //with imgui do this when activate the option to the game object selected or something like that.
-	{
-		SetCheckersToGOSelected();
-	}
+
 	for (list<GameObject*>::iterator item = objects.begin(); item != objects.end(); ++item)
 	{
 		(*item)->Update();
@@ -28,21 +26,6 @@ bool GameObjectManager::Update() //dt?
 	Hierarchy();
 
 	return true;
-}
-
-void GameObjectManager::SetCheckersToGOSelected()
-{
-	for (std::vector<GameObject*>::iterator iter = App->game_object_manager->selected.begin(); iter != App->game_object_manager->selected.end(); ++iter)
-	{
-		if ((*iter)->GetMesh()->GetTexture() != App->texture->GetCheckersTexture())
-		{
-			(*iter)->GetMesh()->SetTexture(App->texture->GetCheckersTexture());
-		}
-		else
-		{
-			(*iter)->GetMesh()->SetTexture((*iter)->GetTextureActivated());
-		}
-	}
 }
 
 bool GameObjectManager::CleanUp()
