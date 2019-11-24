f6b0c1bb711729b8b3bd167707b13e9e6fc542cd
Jorge <jgherencia@gmail.com>
2019-11-24 19:40:42 +0100

    key 1 to create bbox

--- Weep Engine/ComponentTransform.cpp
+++ Weep Engine/ComponentTransform.cpp
@@ -77,7 +77,8 @@ const void ComponentTransform::SetScale(const float3 & scale)
 void ComponentTransform::RecalculateLocalTransform()
 {
 	local_transform = float4x4::FromTRS(local_position, local_rotation_quat, local_scale);
-
+	//if(object->IsStatic() && App->quadtree->quadtree_dynamic)
+	//	App->quadtree->to_recalculate = true;
 }
 
 const void ComponentTransform::SetRotationQuat(const Quat & quat)
